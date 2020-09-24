497b8c0f7404843c0895e96bfbf3ce89b776dc93
DanielSilva23 <dannysilva12345@gmail.com>
2020-09-24 12:35:31 -0400

    templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, render_template
 from flask_restful import Api, Resource, reqparse
 
 from flask_sqlalchemy import SQLAlchemy
@@ -11,10 +11,40 @@ api = Api(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 # db = SQLAlchemy(app)
 
+@app.route("/")
+def home():
+    return render_template("base.html")
 
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
 api.add_resource(video.Video, "/video/<int:video_id>")
 api.add_resource(video.AllVideos, "/videos")
+
+@app.route("/<name>")
+def user(name):
+    return render_template("names.html", name=name, all=['Daniel', 'yo', 'yo soy', 'afa'])
+
+@app.route('/new_name')
+def new_name():
+    return render_template('new_name.html', all=['Daniel', 'yo', 'yo soy', 'afa'] )
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/angular')
+def angular():
+    return render_template('index.html')
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'This is the default route'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
